7b4caffcf1950913a6a567e40419016cab2eed0e
axellegupta <aguptaad@connect.ust.hk>
2017-09-05 16:58:42 +0800

    Wikipedia | A Fallback

--- class12school/scraping.py
+++ class12school/scraping.py
@@ -11,13 +11,11 @@ def selectFunction(find, urls):
             if newData!="sorry":
                 return newData
 
-        # if "wolfram" in url:
-        #     newData = wolfram(find)
-        #     if newData!="sorry":
-        #         return newData
-
-        # else: 
-        #     return "sorry, can't find"
+        if "wikipedia" in url:
+            newData = wikipedia(find)
+            if newData!="sorry":
+                return newData
+        
     return urls, ["Couldn't find results for your query anywhere :(", "You might wanna add another website request for scraping :)"]
 
 def mathsisfun(find):
@@ -60,6 +58,34 @@ def mathsisfun(find):
 
         return url, ["uh oh, nothing here"]
         
+
+    except:
+        return url, ["uh oh, nothing here"]
+
+
+def wikipedia(find):
+    try:
+        findit = find.replace(' ', '_')
+        url = "https://en.wikipedia.org/wiki/"+findit
+        r  = requests.get(url)
+        data = r.text
+        soup = BeautifulSoup(data, 'html.parser')
+        soupGet = soup.get_text()
+
+        if (find.lower() in ((unicodedata.normalize('NFKD', soupGet).encode('ascii','ignore')).lower())):
+            
+            soupGet = soup.find(id = 'mw-content-text').text
+            newData = (unicodedata.normalize('NFKD', soupGet).encode('ascii','ignore').lstrip()).split('\n')
+
+            while '' in newData:
+                newData.remove('')
+                                
+            if not isinstance(newData, list):
+                newData = [newData]
+
+            return url, newData
+
+        return url, ["uh oh, nothing here"]
 
     except:
         return url, ["uh oh, nothing here"]
