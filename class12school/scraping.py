7a0c878429c4835602024fa7e4b188fe011fe6f1
axellegupta <aguptaad@connect.ust.hk>
2017-09-06 00:41:12 +0800

    it's super slow | dunno why I did it | needs to be made parallel, or faster - recursion? or lambda?
    it's super slow | dunno why I did it | needs to be made parallel, or
    faster - recursion? or lambda? … think of other options when less sleepy

--- class12school/scraping.py
+++ class12school/scraping.py
@@ -3,7 +3,7 @@ import re
 from urlparse import urlparse
 import requests
 import unicodedata
-from itertools import combinations
+import itertools
 
 def selectFunction(discover, urls, counter, sub):
     try:
@@ -13,27 +13,33 @@ def selectFunction(discover, urls, counter, sub):
                 if newData[0]!="uh oh, nothing here":
                     print (urlx, newData)
                     return urlx, newData
+
+            # if "mathsisfun" in urls[counter]:
+            #     urlx, newData = wolfram(discover)
+            #     if newData[0]!="uh oh, nothing here":
+            #         print (urlx, newData)
+            #         return urlx, newData ...
                     
                 else:
                     counter+=1
                     return selectFunction(discover, urls, counter, sub)
                     
             else:
-                print ("out of math")
+
                 return selectFunction(discover, urls, 0, "general")
 
         elif ("wikipedia" in urls[counter]) and (counter<len(urls)):
-            print ("here")
+
             urlx, newData = wikipedia(discover)
-            print ("there")
+
             if newData[0]!="uh oh, nothing here":
-                print (urlx, newData)
+
                 return urlx, newData
 
-            # else:
-            #     print ("in else")
-            #     counter+=1
-            #     selectFunction(discover, urls, counter, "general")
+            else:
+                print ("in else")
+                counter+=1
+                selectFunction(discover, urls, counter, "general")
 
         elif (counter<len(urls)):
             print ("waiting")
@@ -46,7 +52,7 @@ def selectFunction(discover, urls, counter, sub):
         return urls, ["Couldn't discover results for your query anywhere :(", "You might wanna add another website request for scraping :)"]
 
 def mathsisfun(discover):
-    discoverit = discover.replace(' ', '+') 
+    discoverit = discover.replace(' ', '+').replace('\'s', '').replace('!', '')
     url = "https://www.mathsisfun.com/sphider/search.php?query="+discoverit+"&type=and&results=1&search=1"
     try: 
         r  = requests.get(url)
@@ -113,7 +119,8 @@ def mathsisfun(discover):
 
 def wikipedia(discover):
     
-    discoverit = discover.replace(' ', '_')
+    discoveryBay = discover
+    discoverit = discover.replace(' ', '_').replace('\'', '%27').replace('!', '').replace('!', '')
     url = "https://en.wikipedia.org/wiki/"+discoverit
 
     try:
@@ -147,12 +154,23 @@ def wikipedia(discover):
 
         except:
             print ("wiki direct don't work")
+        
+        if (' ' in discoveryBay):
+            discover = discoveryBay.split()
 
-        if (' ' in discover):
-            discover = discover.split()
-            for discover in list(combinations(discover, len(discover)-1)):
+            combos = itertools.permutations(discover, len(discover)-1)
+
+            for discover in combos:
+                print discover
+                discoverit =" "
+
+                newD = list(discover)
+
+                for word in newD:
+                    discoverit=str(discoverit)+' '+str(word)
                 
-                discoverit = discover.replace(' ', '_')
+                
+                discoverit = discoverit.lstrip().replace(' ', '_').replace('\'', '%27')
                 url = "https://en.wikipedia.org/wiki/"+discoverit
                 r  = requests.get(url)
                 data = r.text
@@ -160,11 +178,12 @@ def wikipedia(discover):
                 soupGet = soup.get_text()
                 
                 if '_' in discoverit:
-                    discover = discover.split()[0]
-                if ("Wikipedia does not have an article with this exact name".lower() in ((unicodedata.normalize('NFKD', soupGet).encode('ascii','ignore')).lower())):
-                    raise Exception('This is the exception you expect to handle')
+                    discoverit = discoverit.split()[0]
 
-                elif (discover.lower() in ((unicodedata.normalize('NFKD', soupGet).encode('ascii','ignore')).lower())):
+                if ("Wikipedia does not have an article with this exact name".lower() in ((unicodedata.normalize('NFKD', soupGet).encode('ascii','ignore')).lower())):
+                    print ('next')
+
+                elif (discoverit.lower() in ((unicodedata.normalize('NFKD', soupGet).encode('ascii','ignore')).lower())):
                     
                     soupGet = soup.find(id = 'mw-content-text').text
                     newData = (unicodedata.normalize('NFKD', soupGet).encode('ascii','ignore').lstrip()).split('\n')
@@ -176,8 +195,10 @@ def wikipedia(discover):
                         newData = [newData]
 
                     return url, newData
-            else:
-                raise Exception('This is the exception you expect to handle')
+                else:
+                    continue
+        else:
+            raise Exception('This is the exception you expect to handle')
 
     except:
         return url, ["uh oh, nothing here"]
